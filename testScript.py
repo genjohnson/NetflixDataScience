356451ee8ff5be3eddf28dc3aaeafd2c7357bfba
Boogun Choi <bochoi@serv15.divms.uiowa.edu>
2016-04-12 09:23:32 -0500

    Test Script updated for the variaence of the k

--- testScript.py
+++ testScript.py
@@ -50,97 +50,111 @@ m,n = u.shape
 
 import random
 # ID of the User to return recommendations for.
-totalCounter = 0
-counter = 0
-total3 = 0
-total4 = 0
-total5 = 0
+
+# total number of movies that has ratings that we could check on
+
+# counter to iterate over the loop for thousand times
+
+outerCounter = 1
 while True:
-	i = random.randint( 1, 943 )
-	#print("user id : " + str( i ))
-	f = []
+	totalCounter = 0
+	total3 = 0
+	total4 = 0
+	total5 = 0
+	counter = 0
+	while True:
+		i = random.randint( 1, 943 )
+		#print("user id : " + str( i ))
+		f = []
 	
 
-	for x in range(0, len(users)-1):
-		if( users[x] == i ):
-			f.append( x )	
+		for x in range(0, len(users)-1):
+			if( users[x] == i ):
+				f.append( x )	
 
-	mf = []
-	rf = []
-	for x in f: 
-		mf.append( movies[ x] )
-		rf.append( ratings[ x ] )
+		mf = []
+		rf = []
+		for x in f: 
+			mf.append( movies[ x] )
+			rf.append( ratings[ x ] )
 	
 	
-	#print(f)	
-	#print(mf)
-	#print(rf)
-	#print(tf)
-	#print(tmf)
-	#print(trf)
+		#print(f)	
+		#print(mf)
+		#print(rf)
+		#print(tf)
+		#print(tmf)
+		#print(trf)
 	
-	data = np.array([1])
-	columns = np.array([i-1])
-	rows = np.array([0])
+		data = np.array([1])
+		columns = np.array([i-1])
+		rows = np.array([0])
 
-	et = scipy.sparse.csr_matrix((data, (rows, columns)), shape=(1,m), dtype='d')
+		et = scipy.sparse.csr_matrix((data, (rows, columns)), shape=(1,m), dtype='d')
 
-	# Multiply all of the matrices.
-	wt = et.dot(u)
-	wt = wt.dot(sig)
-	wt = wt.dot(vt)
+		# Multiply all of the matrices.
+		wt = et.dot(u)
+		wt = wt.dot(sig)
+		wt = wt.dot(vt)
 
-	# Get the title of the top 10 movies.
-	w = wt.flatten()
-	mids_sorted = np.argsort(w)
-	top_recommendations = mids_sorted[:10]
+		# Get the title of the top 10 movies.
+		w = wt.flatten()
+		mids_sorted = np.argsort(w)
+		top_recommendations = mids_sorted[:10]
 	
-	movie3Rating = 0
-	movie4Rating = 0
-	movie5Rating = 0
-	ct = 0
-	for x in top_recommendations: 
-		index = -1 
-		for y in range(0, len(mf) -1 ):
-			if( x == int(mf[ y ]) ):
-				index = y
-				#print("found movie " + str(x) +  " in base") 
-				break	
+		movie3Rating = 0
+		movie4Rating = 0
+		movie5Rating = 0
+		ct = 0
+		for x in top_recommendations: 
+			index = -1 
+			for y in range(0, len(mf) -1 ):
+				if( x == int(mf[ y ]) ):
+					index = y
+					#print("found movie " + str(x) +  " in base") 
+					break	
 
-		if index == -1  :
-			#print("no movie " + str(x) + " in the data")
-			continue
-		else:
-			ct += 1
-			rating = int(rf[ index ])
-
-			if rating >= 3 :
-				movie3Rating += 1
-			if rating >= 4:
-				movie4Rating += 1
-			if rating ==5 :
-				movie5Rating += 1
-
-
-
-	#print("Counter : " + str(ct) + " with user# " + str(i) )
-	if ct > 0 :
-		#print("movie3Ct: " + str(movie3Rating) + " movie4Ct: " + str(movie4Rating) + " movie5Ct: " + str(movie5Rating))
-		#print("3 or above : " + str(movie3Rating / float(ct) * 100 ) )
-		#print("4 or above : " + str(movie4Rating / float(ct) * 100 ) )
-		#print("5	  : " + str(movie5Rating / float(ct) * 100 ) )	
-		total3 += movie3Rating
-		total4 += movie4Rating
-		total5 += movie5Rating
-		totalCounter += ct
-	#print()
+			if index == -1  :
+				#print("no movie " + str(x) + " in the data")
+				continue
+			else:
+				ct += 1
+				rating = int(rf[ index ])
 	
-	counter += 1		
-	if( counter == 1000 ):
+				if rating >= 3 :
+					movie3Rating += 1
+				if rating >= 4:
+					movie4Rating += 1
+				if rating ==5 :
+					movie5Rating += 1
+
+
+
+		#print("Counter : " + str(ct) + " with user# " + str(i) )
+		if ct > 0 :
+			#print("movie3Ct: " + str(movie3Rating) + " movie4Ct: " + str(movie4Rating) + " movie5Ct: " + str(movie5Rating))
+			#print("3 or above : " + str(movie3Rating / float(ct) * 100 ) )
+			#print("4 or above : " + str(movie4Rating / float(ct) * 100 ) )
+			#print("5	  : " + str(movie5Rating / float(ct) * 100 ) )	
+			total3 += movie3Rating
+			total4 += movie4Rating
+			total5 += movie5Rating
+			totalCounter += ct
+		#print()
+	
+		counter += 1		
+		if( counter == 15 ):
+			break
+	
+
+	print( "for k = " + str(outerCounter) )
+	print("3 or above : " + str( total3 / float(totalCounter) * 100 ))
+	print("4 or above : " + str( total4 / float(totalCounter) * 100 ))
+	print("5 or above : " + str( total5 / float(totalCounter) * 100 ))
+
+	outerCounter += 1
+	if( outerCounter == 150 ) : 
 		break
-	
-print("3 or above : " + str( total3 / float(totalCounter) * 100 ))
-print("4 or above : " + str( total4 / float(totalCounter) * 100 ))
-print("5 or above : " + str( total5 / float(totalCounter) * 100 ))
+
 #for i in top_recommendations:
 #  movieTitleLookup(i)
